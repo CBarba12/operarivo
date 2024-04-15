1422c4f95a5d8e9861b6bd53b2c8cfec1cae2403
CBarba12 <101232758+CBarba12@users.noreply.github.com>
2024-04-14 19:18:07 -0600

    cambiosa

--- disco.c
+++ disco.c
@@ -73,11 +73,7 @@ void mostrarEspacioDisco(long total, long usado, long libre, int m) {
 }
 
 int obtenerDatosDisco(int unidad) {
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <ctype.h>
-#include <unistd.h> 
+
 
     FILE *archivo = popen("df -BM /", "r");
     if (archivo == NULL) {
